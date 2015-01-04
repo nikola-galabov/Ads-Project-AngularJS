7a32b1d391d5bcdc4ed7661609e47db778b85bec
Nikola Galabov <n.v.galabov@gmail.com>
2015-01-04 11:47:35 +0200

    make the views

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,19 +1,3 @@
-app.controller('Main', function($scope, $http){
-	
-	$scope.getAllAds = function getAllAds(success, error) {
-		$http({
-			method: 'GET',
-			url: 'http://softuni-ads.azurewebsites.net/api/ads' // Try with invalid url
-			// headers: {}
-			// data: {}
-		})
-		.success(function (data, status, headers, config) {
-			success(data, status, headers(), config);
-		})
-		.error(function (data, status, headers, config) {
-			error(data, status, headers(), config);
-		});
-	}
-
+app.controller('Main', function AdsController($scope) {
 	
 });
